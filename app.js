9b34f665f56d8ccd4cb9884f4eaf05d286b40972
kaushik94 <kaushik.varanasi1@gmail.com>
2015-11-23 13:41:51 +0530

    binding to a port, resolving the heroku port issues

--- app.js
+++ app.js
@@ -27,4 +27,12 @@ worker.collect(params);
 
 pusher.push();
 
-logger.info("Listening on port " + port);
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
